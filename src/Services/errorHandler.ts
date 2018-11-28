cbc1d7b388c4954183f9de444aa2dd5d5e71c422
Sarunyu Chankong <sarunyu.you@gmail.com>
2018-11-28 20:14:41 +0700

    Add JWT expired error handler

--- src/Services/errorHandler.ts
+++ src/Services/errorHandler.ts
@@ -1,12 +1,21 @@
 import * as Sentry from '@sentry/node'
 import { HTTP} from '../constants/http';
 export const ExpressErrorHandler = (err, req, res, next) => {
-  // console.log(err.code)
+  // console.log(err)
+  if(err.message==="jwt expired"){
+    res.status(HTTP.UNAUTHORIZED).json({
+      success: false,
+      message:  err.message
+    })
+  }
+  else{
+    res.status(err.code||HTTP.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message:  err.message||'Something broke!'
+    })
+  }
 
-  res.status(err.code||HTTP.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message:  err.message||'Something broke!'
-  })
+  
   Sentry.captureException(err)
   
 }
